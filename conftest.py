42e4d81b572d546fa54d18df53f184c0bcdeb0cc
oxakuzmina <oxakuzmina@gmail.com>
2023-07-14 13:30:44 -0400

    Added conftest and test

---
+++ conftest.py
@@ -0,0 +1,28 @@
+from selenium.webdriver.chrome import webdriver
+from selenium.webdriver.chrome.options import Options
+import pytest
+from selenium import webdriver
+
+ACCEPTED_LANGUAGES = ['ar', 'ca', 'cs', 'da', 'de', 'en-gb', 'en', 'el', 'es', 'fi', 'fr', 'it',
+                      'ko', 'nl', 'pl', 'pt', 'pt-br', 'ro', 'ru', 'sk', 'uk', 'zh-hans']
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Please, choose language")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    if language in ACCEPTED_LANGUAGES:
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+        print(f"{language} was selected")
+    else:
+        raise pytest.UsageError(
+            f"--language should be one from accepted language. Here is the list :{ACCEPTED_LANGUAGES}")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
